9a0e0b0f953f8b904fb08bae4294e761f9950cb8
Michael Conrad <mike@nrdvana.net>
2019-03-31 00:35:26 -0400

    Upgrsde chat demo to select username
    Also demonstrates inspecting the HTTP request.

--- anyevent-standalone/chat.pl
+++ anyevent-standalone/chat.pl
@@ -6,7 +6,13 @@ use AnyEvent::Socket qw(tcp_server);
 use AnyEvent::WebSocket::Server;
 
 my %connections;
-my $server= AnyEvent::WebSocket::Server->new();
+my $server= AnyEvent::WebSocket::Server->new(
+	handshake => sub {
+		my ($req, $res)= @_;
+		my ($username)= ($req->resource_name =~ /username=([^&]*)/);
+		return $res, $username;
+	},
+);
 
 # Listen on port 5000 for incoming connections
 my $next_id= 1;
@@ -14,25 +20,30 @@ tcp_server undef, 5000, sub {
 	my ($fh)= @_;
 	# For each connection, hand off to $server to talk HTTP and upgrade to websocket
 	$server->establish($fh)->cb(sub {
-		my ($conn)= eval { shift->recv };
+		my ($conn, $username)= eval { shift->recv };
 		if ($@) {
 			warn "Rejected connection: $@\n";
 			close($fh);
 			return;
 		}
-		# Ignore the HTTP details of the request, and accept the client as a new chat user
-		$connections{$conn}= $conn;
-		$conn->{client_id}= $next_id++;
-		$conn->on(each_message => sub { my @args= @_; eval { handle_message(@args); 1 } || warn $@ });
-		$conn->on(finish => sub { delete $connections{$conn}; undef $conn });
+		# If username is taken, reject with a message
+		if ($connections{$username}) {
+			$conn->send("Username is taken");
+			$conn->disconnect;
+		}
+		else {
+			$connections{$username}= $conn;
+			$conn->on(each_message => sub { my @args= @_; eval { handle_message($username, @args); 1 } || warn $@ });
+			$conn->on(finish => sub { delete $connections{$username}; });
+		}
 	});
 };
 
 sub handle_message {
-	my ($conn, $msg)= @_;
+	my ($username, $conn, $msg)= @_;
 	# Messages can be several different types.  We only care about text.
 	if ($msg->is_text) {
-		my $text= 'Client'.$conn->{client_id}.': '.$msg->decoded_body;
+		my $text= $username . ': ' . $msg->decoded_body;
 		# re-broadcast message to every connection, prefixed with connection name
 		$_->send($text) for values %connections;
 	}
